af8378dfff34e4c717a4cb11683f9bc9a8a71a0b
Sarthak Jain <sarthakjain31797@gmail.com>
2022-10-11 02:21:27 +0530

    Add Header Page Template

--- digitalShop/views.py
+++ digitalShop/views.py
@@ -2,4 +2,4 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def index(request):
-    return HttpResponse("<h1>Home Page</h1>")
+    return render(request,'index.html')
