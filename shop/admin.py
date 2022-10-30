c05b76a3cf0fa4eb536ddb7ade6b9aefb303ff97
Sarthak Jain <sarthakjain31797@gmail.com>
2022-10-31 00:36:31 +0530

    Add DashBoard Tags DataBase Model

--- shop/admin.py
+++ shop/admin.py
@@ -3,10 +3,12 @@ from django.contrib import admin
 from .models import Products
 from .models import Cart
 from .models import WishList
+from .models import DeshboardTags
 from .models import Deshboard
 
 # Register your models here.
 admin.site.register(Products)
 admin.site.register(Cart)
 admin.site.register(WishList)
+admin.site.register(DeshboardTags)
 admin.site.register(Deshboard)
--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,5 @@
+from email.policy import default
+from unittest.util import _MAX_LENGTH
 from django.db import models
 from django.conf import settings
 import uuid
@@ -24,7 +26,16 @@ class WishList(models.Model):
     buyer = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.DO_NOTHING)
     product_id = models.ForeignKey('Products',on_delete=models.DO_NOTHING)
 
+# These Tags will display on Deshboard
+class DeshboardTags(models.Model):
+    tag = models.CharField(max_length=100)
+    rank = models.IntegerField()
+    
+    def __str__(self):
+        return self.tag
+
 class Deshboard(models.Model):
+    deshboard = models.ForeignKey('DeshboardTags',on_delete=models.DO_NOTHING) 
     deshboard_Product_Catelog = models.ImageField(upload_to='media/deshboard',default='media/deshboard/product.jpg')
     deshboard_Product_Name = models.CharField(max_length=50)
     deshboard_Product_Description = models.TextField(blank=True, null=True)
