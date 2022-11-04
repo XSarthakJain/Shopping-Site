3f0f2836d8ef6c04a505bd7232c75d9b02839d46
Sarthak Jain <sarthakjain31797@gmail.com>
2022-11-05 00:44:37 +0530

    Add Quantity Attribute and Set Validation min value can not be lower than 0

--- shop/models.py
+++ shop/models.py
@@ -3,6 +3,7 @@ from unittest.util import _MAX_LENGTH
 from django.db import models
 from django.conf import settings
 import uuid
+from django.core.validators import MinValueValidator
 # Create your models here.
 class Products(models.Model):
     product_id = models.UUIDField(primary_key = True,default = uuid.uuid4,editable = False)
@@ -13,6 +14,8 @@ class Products(models.Model):
     product_Company = models.CharField(max_length=50)
     product_Category = models.CharField(max_length=50)
     product_desc = models.CharField(max_length=200)
+    product_quantity = models.IntegerField(default=1,validators=[
+        MinValueValidator(0)])
 
     def __str__(self):
         return self.product_Name
